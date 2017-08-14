eb02f307416fb26357e9da9156a60a42335ecba4
marynganga <marynganga.mw@gmail.com>
2017-08-14 04:06:46 +0300

    Divide code in scripts.js into business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
+//BUSINESS OR NACK-END LOGIC
 var add = function(number1, number2) {
   return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var addresult = add(number1, number2);
-alert(addresult);
 
 var minus = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var minusresult = minus(number1, number2);
-alert(minusresult);
-
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var multiplyresult = multiply(number1, number2);
-alert(multiplyresult);
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var divideresult = divide(number1, number2);
-alert(divideresult);
+//USER-INTERFACE OR FRONT-END LOGIC
+$(document).ready(function() {
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+
+      var addresult = add(number1, number2);
+      alert(addresult);
+
+      /*
+      var minusresult = minus(number1, number2);
+      alert(minusresult);
+
+      var multiplyresult = multiply(number1, number2);
+      alert(multiplyresult);
+
+      var divideresult = divide(number1, number2);
+      alert(divideresult);
+*/
+    }
